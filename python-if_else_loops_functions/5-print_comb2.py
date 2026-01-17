41a9c0aea9b513f7d90910fbc5406da0f9087a47
Your Name <you@example.com>
2026-01-17 20:45:36 +0300

    sloving task 5

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    print("{:02d}".format(i), end="")
+    print("{:02d}{}".format(i, ", " if i != 99 else "\n"), end="")
     if i != 99:
         print(", ",end="")
