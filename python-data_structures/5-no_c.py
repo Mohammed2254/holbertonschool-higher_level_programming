bd6c83102ddeb782aae7ba643d9000b9133cec82
Your Name <you@example.com>
2026-01-20 12:06:25 +0300

    fix problems

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -2,8 +2,8 @@
 def no_c(my_string):
     new = ""
     for char in my_string:
-        if char == 'c' or char == 'C':
-            new +=  ""
+        if char == "c" or char == "C":
+            new += ""
         else:
             new += char
     return new
