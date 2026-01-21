549b0d839873fc36275eff2e764b632718c61fe2
Your Name <you@example.com>
2026-01-21 23:56:56 +0300

    solving tasks

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary == None or a_dictionary == {}:
+    if a_dictionary is None or a_dictionary == {}:
         return None
     best = ""
     val = 0
