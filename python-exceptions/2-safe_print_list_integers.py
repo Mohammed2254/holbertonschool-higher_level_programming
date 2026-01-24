4542a0f6b1f863f8a8f94866beda344599f8fa05
Your Name <you@example.com>
2026-01-24 23:59:23 +0300

    g

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -9,14 +9,12 @@ def safe_print_list_integers(my_list=[], x=0):
         Real number of integers printed
     """
     count = 0
-    try:
-        for i in range(x):
-            try:
-                print("{:d}".format(my_list[i]), end="")
-                count += 1
-            except (ValueError, TypeError):
-                continue
-    except IndexError:
-        pass
-    print()
+
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
+        except (ValueError, TypeError):
+            continue
+
     return count
