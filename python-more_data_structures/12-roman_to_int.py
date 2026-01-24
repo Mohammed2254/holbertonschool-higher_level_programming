2e1b3401214514a6314d69c2ca8bcc6648484573
Your Name <you@example.com>
2026-01-24 23:51:55 +0300

    g

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -9,11 +9,11 @@ def roman_to_int(roman_string):
     """
     if not roman_string or not isinstance(roman_string, str):
         return 0
-    roman_dict = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    romdict={"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     total = 0
     prev_value = 0
     for char in reversed(roman_string):
-        current_value = roman_dict.get(char, 0)
+        current_value = romdict.get(char, 0)
         if current_value >= prev_value:
             total += current_value
         else:
