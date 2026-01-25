a8b66a15bc736c722d9cce4cbb94930fa773f5f9
Your Name <you@example.com>
2026-01-25 19:24:44 +0300

    task 0 - 4

---
+++ python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" define a class"""
+
+class Square:
+     """square class"""
+    def __init__(self, size=0):
+        """main things in this class"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
---
+++ python-classes/4-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" define a class"""
+
+class Square:
+    """square class"""
+    def __init__(self, size=0):
+        """main things in this class"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+    def area(self):
+        """Return the current area of the square."""
+        return self.__size * self.__size
+    
