668dcc4babf8965a8e1356833ba5e77b5ea97fde
Your Name <you@example.com>
2026-01-25 18:50:36 +0300

    task 0 and 1

---
+++ python-classes/0-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+a model that def class
+"""
+
+
+class Square:
+    """class name square"""
+
+    pass
---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" define a class"""
+
+
+class Square:
+    """square class"""
+
+    def __init__(self, size):
+        """main things in this class"""
+        self.__size = size
