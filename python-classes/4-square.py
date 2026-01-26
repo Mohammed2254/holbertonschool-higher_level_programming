5b90fb7806e5af77dc114a3875a01b41e4cbb048
Your Name <you@example.com>
2026-01-26 16:47:05 +0300

    solving bugs in task 4

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
-""" define a class"""
+"""Define a class Square."""
 
 class Square:
-    """square class"""
     def __init__(self, size=0):
-        """main things in this class"""
-        if not isinstance(size, int):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        elif size < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        self.__size = value
 
     def area(self):
-        """Return the current area of the square."""
         return self.__size * self.__size
-    
