f517ecb289e51f513e38509bc059d8ae3c0b0189
Your Name <you@example.com>
2026-01-26 16:50:21 +0300

    tasl 4

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,16 +1,34 @@
 #!/usr/bin/python3
 """Define a class Square."""
 
+
 class Square:
+    """Represent a square."""
+
     def __init__(self, size=0):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
         self.size = size
 
     @property
     def size(self):
+        """Get/retrieve the current size of the square."""
         return self.__size
 
     @size.setter
     def size(self, value):
+        """Set the size of the square.
+
+        Args:
+            value (int): The new size.
+
+        Raises:
+            TypeError: If value is not an int.
+            ValueError: If value is < 0.
+        """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
@@ -18,4 +36,5 @@ class Square:
         self.__size = value
 
     def area(self):
+        """Return the current area of the square."""
         return self.__size * self.__size
