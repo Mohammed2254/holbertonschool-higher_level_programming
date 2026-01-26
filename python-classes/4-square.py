684da9255ecb3ed6849e89e2455aec45f83dc153
Your Name <you@example.com>
2026-01-26 16:56:59 +0300

    task 4

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -11,6 +11,10 @@ class Square:
         Args:
             size (int): The size of the new square.
         """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")    
         self.size = size
     def area(self):
         """Return the current area of the square."""
