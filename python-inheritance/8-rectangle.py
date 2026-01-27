d2e38ffee27089049e8620c72b33d454aa267b69
Your Name <you@example.com>
2026-01-27 16:47:41 +0300

    solving bugs in task 8-10

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -4,7 +4,7 @@ Module that defines a Rectangle class inheriting from BaseGeometry.
 """
 
 
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
 
 class Rectangle(BaseGeometry):
