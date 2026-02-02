1913395b2e477bdf2c99e5073758e6503503a8fd
Your Name <you@example.com>
2026-02-02 23:26:16 +0300

    solving tasks 0-11 v3

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ module for 1 function"""
-
+import json
 
 def from_json_string(my_str):
     """the func"""
--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
+"""module"""
 class Student:
+    """class"""
     def __init__(self, first_name, last_name, age):
+        """main obj"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
+        """to"""
         return self.__dict__
