1a90cb9989292ee0d88f3f6e0e0b76f83817047a
Mohammed2254 <m.a.alabduli@gmail.com>
2026-02-07 23:49:46 +0300

    task 2

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -2,5 +2,19 @@
 import csv
 import json
 
-def convert_csv_to_json(csvfile, data):
-    pass
+def convert_csv_to_json(csvfile):
+    """ff """
+    try:
+
+        with open(csvfile) as file:
+            reader = csv.DictReader(file)
+            data =[]
+            for row in reader:
+                data.append(row)
+            with open("data.json", "w") as file:
+                json.dump(data, file)
+        return True
+
+
+    except:
+        return False
