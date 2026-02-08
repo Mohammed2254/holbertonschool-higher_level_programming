c62e652f1deb8c66ffd1c4a7e7a550ec40a7564d
Mohammed2254 <m.a.alabduli@gmail.com>
2026-02-08 22:17:50 +0300

    solve status code problems

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -7,8 +7,8 @@ import csv
 
 def fetch_and_print_posts():
     data = requests.get("https://jsonplaceholder.typicode.com/posts")
-    print(f"status code :{data.status_code}")
-    if data.status_code == 200:
+    print(f"Status Code :{data.status_code}")
+    if data.ok:
         data = data.json()
         for item in data:
             print(f"{item['title']}")
@@ -16,7 +16,7 @@ def fetch_and_print_posts():
 
 def fetch_and_save_posts():
     data = requests.get("https://jsonplaceholder.typicode.com/posts")
-    if data.status_code == 200:
+    if data.ok:
         data = data.json()
         listOfData = []
         for itm in data:
