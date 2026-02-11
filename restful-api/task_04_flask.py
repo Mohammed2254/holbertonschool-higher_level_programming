f4fc210d75dbd24548c09ff9d92cd338d4742dda
Mohammed2254 <m.a.alabduli@gmail.com>
2026-02-11 14:51:43 +0300

    solving task 4

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -1,34 +1,57 @@
 """first flask thing in my entire life"""
+
 from flask import Flask
 from flask import jsonify
 from flask import request
 import json
 
+users = {}
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def home():
+    """main"""
     return "Welcome to the Flask API!"
 
-@app.route('/data')
-def res():
-    return list(users.keys())
 
-@app.route('/status')
+@app.route("/data")
+def res():
+    """"""
+    return jsonify(list(users.keys()))
+
+
+@app.route("/status")
 def sta():
-    return jsonify({"status": "OK"})
+    """"""
+
+    return "OK"
+
 
 @app.route("/users/<username>")
 def get_user(username):
-    user = users.get(username)
+    """G"""
+    user = user.get(username)
     if user is None:
         return jsonify({"error": "User not found"}), 404
     return jsonify(user)
 
-@app.route("/add_user")
-def add_user(data):
-    dictUser = json.load(data)
-    users[username] = dictUser
-    
-if __name__ == "__main__": 
-    app.run("localhost",port=5000)
+
+@app.route("/add_user", methods=["POST"])
+def add_user():
+    """g"""
+    dictForUser = request.get_json(silent=True)
+    if not dictForUser or not isinstance(dictForUser, dict):
+        return jsonify({"error": "Invalid JSON"}), 400
+    username = dictForUser["username"]
+    if "username" not in dictForUser:
+        return jsonify({"error": "Username is required"}), 400
+    if username in dictForUser:
+        return jsonify({"error": "Username already exists"}), 409
+
+    users[username] = dictForUser
+    return jsonify({"message": "User added", "user": dictForUser})
+
+
+if __name__ == "__main__":
+    app.run("localhost", port=5000)
